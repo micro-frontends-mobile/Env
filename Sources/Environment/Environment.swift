5fa729b9d3f8bbffddc36c9587d1d0b2068e0113
zdd <zddhub@gmail.com>
2022-05-22 20:08:49 +0800

    Add router to env

--- Sources/Environment/Environment.swift
+++ Sources/Environment/Environment.swift
@@ -1,6 +1,27 @@
-public struct Environment {
-    public private(set) var text = "Hello, World!"
+//
+//  Environment.swift
+//
+//
+//  Created by Dongdong Zhang on 2022/5/22.
+//
 
-    public init() {
+import SwiftUI
+
+public class Environment: ObservableObject {
+  public private(set) static var shared: Environment!
+
+  public static func initialize(router: Router = Router()) -> Environment {
+    if shared != nil {
+      return shared
     }
+
+    shared = Environment(router: router)
+    return shared
+  }
+
+  init(router: Router) {
+    self.router = router
+  }
+
+  public let router: Router
 }
