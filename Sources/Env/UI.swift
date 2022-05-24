468f46c88007b01022e9baeb766f60e0d9d4ff90
zdd <zddhub@gmail.com>
2022-05-24 23:11:31 +0800

    Use gray as background for dark mode

--- Sources/Env/UI.swift
+++ Sources/Env/UI.swift
@@ -14,7 +14,7 @@ enum UI {
       case .backgroundColor:
         return Color(uiColor: duality(
           light: UIColor(red: 243/255, green: 243/255, blue: 222/255, alpha: 1), // #f3f3de
-          dark: UIColor(red: 134/255, green: 112/255, blue: 24/255, alpha: 1) // #867018
+          dark: .gray
         ))
       case .tabBarTintColor:
         return Color(uiColor: duality(
